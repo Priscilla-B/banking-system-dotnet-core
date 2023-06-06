ae98fb9d62f17731f31aca2b0ff30b150609b2be
Priscilla Baah <pyfbaah@gmail.com>
2023-06-06 16:57:58 +0000

    add login actions

---
+++ BankingSystem/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankingSystem.ViewModels.Auth;
+
+namespace BankingSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel obj)
+        {
+            return View();
+        }
+    }
+}
